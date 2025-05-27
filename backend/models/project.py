8ac3fb10e0a22ed1ae541eba7ada3d0f9b09717b
1Gireesh <23f2005217@ds.study.iitm.ac.in>
2025-05-27 20:26:07 +0530

    modified database models

--- backend/models/project.py
+++ backend/models/project.py
@@ -1,16 +1,21 @@
 from extensions import db
+from datetime import datetime, timezone
 
 class Membership(db.Model):
     __tablename__ = 'membership'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    isEditor = db.Column(db.Boolean, default=False, nullable=False)
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    deadline = db.Column(db.DateTime, nullable=True)
+    project_image = db.Column(db.String(255),default="https://cdn-icons-png.flaticon.com/512/1087/1087927.png", nullable=True) 
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     
     # Relationships
     owner = db.relationship('User', backref='owned_projects', foreign_keys=[owner_id])
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,14 +1,17 @@
 from extensions import db, bcrypt
+from datetime import datetime, timezone
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=True)  # Make nullable for Google OAuth users
-    google_id = db.Column(db.String(100), unique=True, nullable=True)  # Add Google ID
+    google_id = db.Column(db.String(100), unique=True, nullable=True)  # Add = Google ID
     profile_picture = db.Column(db.String(255), nullable=True)  # Add profile picture URL
     notify_email = db.Column(db.Boolean, default=True)
     notify_in_app = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
     
     # Add the projects relationship that corresponds to the 'members' relationship in Project
     projects = db.relationship('Project', secondary='membership', back_populates='members')
